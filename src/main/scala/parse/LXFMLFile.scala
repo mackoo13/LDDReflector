dd09c4ca5ff6405cdcca6aba9d8eb364066b58b9
Maciej Kocot <mkocot@op.pl>
2016-05-19 16:01:32 +0200

    matrix operations

--- src/main/scala/parse/LXFMLFile.scala
+++ src/main/scala/parse/LXFMLFile.scala
@@ -1,4 +1,8 @@
 package parse
+import utils.TransformationMatrix
+
+import scala.xml.{Elem, Node, NodeSeq}
+import scala.xml.transform.{RewriteRule, RuleTransformer}
 
 class LXFMLFile(val filename: String) {
   println("open "+filename)
@@ -16,6 +20,21 @@ object HelloWorld {
     println((file.source \ "bla").map(_.text).mkString("+"))
     println((file.source \ "bla").map(_ \ "@t").mkString("+"))
     println((file.source \ "bla").map(_ \ "@t").map(_.text.split(",").mkString("_")).mkString("="))
+
+    val removeBone = new RewriteRule {
+      override def transform(n: Node): NodeSeq = n match {
+//        case e: Elem if (e \ "@t").text == "1,2,3" => NodeSeq.Empty
+        case e: Elem if e.label == "fuj" => NodeSeq.Empty
+        case _ => n
+      }
+    }
+    println(new RuleTransformer(removeBone).transform(file.source))
+
+    //----------------------
+
+    val matrix = new TransformationMatrix("1,0,0,0,1,0,0,0,1,3,5,8")
+    matrix.reflectedMatrix
+
 
   }
 }
---
+++ src/main/scala/utils/TransformationMatrix.scala
@@ -0,0 +1,38 @@
+package utils
+
+class TransformationMatrix(val input: String, val reflectionAxis: Int = 0) {
+  val matrix = input.split(",")
+  val trans = matrix.slice(0, 9)
+  val pos = matrix.slice(9, 12)
+  val transDouble = trans.map(_.toDouble)
+  val posDouble = pos.map(_.toDouble)
+
+  val toMiddle = Array(2.0, 0.0, 0.4)
+
+  def reflectedMatrix: String = {
+    val newTransString = (List("", "-", "-", "-", "", "", "-", "", ""), trans).zipped.map(_ + _)
+    val newTransDouble = newTransString.map(_.toDouble).toArray
+
+    val currentMidPos = Product(transDouble, posDouble, toMiddle)
+    val wantedMidPos = currentMidPos
+    wantedMidPos(reflectionAxis) *= -1
+    val newMidPos = Product(newTransDouble, Array(0.0, 0.0, 0.0), toMiddle)
+
+    val newPosDouble = Subtraction(wantedMidPos, newMidPos)
+    val newPosString = newPosDouble.map(_.toString)
+
+    println("new trans: "+newTransString.mkString(","))
+    println("new trans: "+newPosString.mkString(","))
+
+    "bla"
+  }
+
+  def Product(trans: Array[Double], pos: Array[Double], coords: Array[Double]): Array[Double] = {
+    val x = trans(0)*coords(0) + trans(3)*coords(1) + trans(6)*coords(2) + pos(0)
+    val y = trans(1)*coords(0) + trans(4)*coords(1) + trans(7)*coords(2) + pos(1)
+    val z = trans(2)*coords(0) + trans(5)*coords(1) + trans(8)*coords(2) + pos(2)
+    Array(x, y, z)
+  }
+
+  def Subtraction(a: Array[Double], b: Array[Double]) = Array(a(0)-b(0), a(1)-b(1), a(2)-b(2))
+}
