74ed70f2e1fbc60c78331c1c6c108ea78311ad5d
Maciej Kocot <mkocot@op.pl>
2016-05-21 09:44:34 +0200

    let's tidy up + csv parsing

--- src/main/scala/parse/LXFMLFile.scala
+++ src/main/scala/parse/LXFMLFile.scala
@@ -4,26 +4,29 @@ import utils.TransformationMatrix
 import scala.xml.{Attribute, _}
 
 class LXFMLFile(val filename: String) {
-  println("open "+filename)
-//  val source = <lol><bla t="1,2,3">1</bla><bla t="2,4,6">2</bla><fuj>fuj</fuj></lol>
-  val source = XML.loadFile(filename)
-}
+  var source: Node = XML.loadFile(filename)
 
+  def parse(): Unit = {
+    source = parseNode(source)
+  }
+
+  def parseNode(n: Node): Node = n match {
+    case e: Elem if e.label=="Bone" =>
+      val matrix = new TransformationMatrix((e \ "@transformation").text)
+      e % Attribute(null, "transformation", matrix.reflectedMatrix, Null)
+    case e: Elem if e.label =="RigidSystems" => e.copy(child = Seq.empty[Node])
+    case e: Elem => e.copy(child = e.child.map(parseNode(_)))
+    case _ => n
+  }
+}
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
 
-    def deepCopy(n: Node): Node = n match {
-      case e: Elem if e.label=="Bone" =>
-        val matrix = new TransformationMatrix((e \ "@transformation").text)
-        e % Attribute(null, "transformation", matrix.reflectedMatrix, Null)
-      case e: Elem if e.label =="RigidSystems" => e.copy(child = Seq.empty[Node])
-      case e: Elem => e.copy(child = e.child.map(deepCopy(_)))
-      case _ => n
-    }
-
     val file = new LXFMLFile("in1.lxfml")
-    println(deepCopy(file.source))
+    file.parse()
+    XML.save("out1.lxfml", file.source)
+    println(file.source)
 
   }
 }
---
+++ src/main/scala/utils/RegisterPoints.scala
@@ -0,0 +1,15 @@
+package utils
+
+class RegisterPoints(val filename: String) {
+
+  val source = io.Source.fromFile(filename)
+  var data = Map[Int, Array[Double]]()
+
+  for (line <- source.getLines) {
+    val cols = line.split(",").map(_.trim)
+    data += (cols(0).toInt -> cols.slice(1,4).map(_.toDouble))
+  }
+
+  source.close
+  println(data)
+}
