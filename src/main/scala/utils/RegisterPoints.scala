0ab3c25ec6625b9c30aadbd14eddba778b8671af
Maciej Kocot <mkocot@op.pl>
2016-05-25 20:55:04 +0200

    auto update

--- src/main/scala/utils/RegisterPoints.scala
+++ src/main/scala/utils/RegisterPoints.scala
@@ -1,32 +1,52 @@
 package utils
 
-import sys.process._
-import java.net.URL
+import java.io.PrintWriter
+import java.net.{MalformedURLException, UnknownHostException}
 import java.io.File
+
 
 class RegisterPoints(val filename: String) {
 
-  try update("http://google.com", filename)
-  catch {
-    case e: Throwable => println(e.getMessage)
-  }
-
-  val source = io.Source.fromFile(filename)
   var data = Map[Int, Array[Double]]()
+  if(!updateData()) loadData()
 
-  for (line <- source.getLines) {
-    val cols = line.split(",").map(_.trim)
-    data += (cols(0).toInt -> cols.slice(1,4).map(_.toDouble))
+  def updateData(): Boolean = {
+    val urlFilename = "registerPointsUpdateURL.txt"
+    try {
+      val url = io.Source.fromFile(urlFilename).mkString
+      println("Loading file from "+url)
+      val source = io.Source.fromURL(url)
+      for (line <- source.getLines) {
+        val cols = line.split(",").map(_.trim)
+        data += (cols(0).toInt -> cols.slice(1,4).map(_.toDouble))
+      }
+      source.close
+      saveData(source.mkString)
+      true
+    } catch {
+      case e: MalformedURLException => println("Wrong URL format in file "+urlFilename); false
+      case e: UnknownHostException => println("Unable to connect to github.com"); false
+    }
   }
 
-  source.close
+  def loadData() = {
+    println("Loading file from "+filename)
+    val source = io.Source.fromFile(filename)
+    for (line <- source.getLines) {
+      val cols = line.split(",").map(_.trim)
+      data += (cols(0).toInt -> cols.slice(1,4).map(_.toDouble))
+    }
+    source.close
+  }
+
+  def saveData(content: String) = {
+    val writer = new PrintWriter(new File(filename))
+    try writer.write(content)
+    finally writer.close()
+  }
 
   def mid(partNo: Int): Array[Double] = data(partNo)
 
   def contains(partNo: Int): Boolean = data.contains(partNo)
 
-  def update(url: String, filename: String) = {
-    println(67)
-    new URL(url) #> new File(filename)
-  }
 }
